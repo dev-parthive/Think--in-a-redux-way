3adaa966a915f102b47efa0fac7a0ae34688bcff
dev-parthive <dev-parthive>
2023-09-09 21:56:25 +0600

    reudcer function created

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("hello world ");
+// / selecte dom elements 
+const counter = document.getElementById("counter")
+const incrementBtn = document.getElementById("increment")
+const decrementBtn = document.getElementById("decrement")
+
+// initial state 
+const initailState  = {
+    value : 0 , 
+
+};
+//  Create Reducer function 
+function counterReducer(state = initailState , action ){
+    action =  {
+        type: "increment"
+    }
+    if(action.type === "increment"){
+        return {...state , value : state.value + 1};
+    } 
+    else if (action.type === "decrement"){
+        return {...state , value : state.value - 0 }
+    }else{
+        return state;
+    }
+}
+
+
+// create store  
+const store = Redux.createStore(counterReducer)
+console.log(store);
+
+
+// button click listener 
+
+
