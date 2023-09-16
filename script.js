55eee3c3c9d8dc545bcf382078167cb32be0acb5
dev-parthive <dev-parthive>
2023-09-16 23:37:11 +0600

    now buttons are working

--- script.js
+++ script.js
@@ -3,32 +3,52 @@ const counter = document.getElementById("counter")
 const incrementBtn = document.getElementById("increment")
 const decrementBtn = document.getElementById("decrement")
 
+
+
 // initial state 
 const initailState  = {
-    value : 0 , 
+    value : 0 
 
 };
 //  Create Reducer function 
 function counterReducer(state = initailState , action ){
-    action =  {
-        type: "increment"
-    }
     if(action.type === "increment"){
+        console.log("value of state", state)
         return {...state , value : state.value + 1};
     } 
     else if (action.type === "decrement"){
-        return {...state , value : state.value - 0 }
+        return {...state , value : state.value - 1 }
     }else{
         return state;
     }
 }
 
 
-// create store  
+// create store 
 const store = Redux.createStore(counterReducer)
-console.log(store);
+// here is the render function 
+const render = () => {
+    const state = store.getState();
+    counter.innerText = state.value.toString()
+    console.log(state.value)
+}
+render()
+// render function subscribed to the store 
+store.subscribe(render)
 
 
 // button click listener 
+incrementBtn.addEventListener("click", ()=>{
+    store.dispatch({
+        type: "increment"
+    })
 
+    console.log("button is clicked");
+})
 
+decrementBtn.addEventListener("click", ()=>{
+    store.dispatch({
+        type: "decrement"
+    })
+    console.log("Decrement Button is clicked ")
+})
