96cfab1016aea7eb39581e482a580fe04d79aa61
dev-parthive <dev-parthive>
2023-10-22 19:02:10 +0600

    initial redux setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-
+import { Provider } from 'react-redux';
 import { useState } from 'react';
 import './App.css'
 import Counter from './Components/Counter'
 import State from './Components/State';
+import store from './Redux/store';
 function App() {
 
   const initialState = [
@@ -24,7 +25,7 @@ const totalCount = ()=>{
 totalCount()
 
 const increment = (id) =>{
-  const updatedConter = state.map(c => {
+  const updatedCounter = state.map(c => {
     if(c.id === id){
       return {
         ...c , 
@@ -34,14 +35,14 @@ const increment = (id) =>{
     return {...c}
   }) 
 
-  setState(updatedConter)
+  setState(updatedCounter)
 
 
 }
 
 
 const decrement = (id) =>{
-  const updatedConter = state.map(c => {
+  const updatedCounter = state.map(c => {
     if(c.id === id){
       return {
         ...c , 
@@ -50,25 +51,24 @@ const decrement = (id) =>{
     }
     return {...c}
   }) ;
-  setState(updatedConter)
+  setState(updatedCounter)
 
 
 }
   return (
     <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-      <h1 className='max-w-md mx-auto text-center text-2xl font-bold'>Simple Counter Application </h1>
-      <div className='max-w-md mx-auto mt-10 space-y-5'>
+            
+            <h1 className="max-w-md mx-auto text-center  text-2xl font-bold">
+                Simple Counter Application
+            </h1>
 
-   {state.map(count =>{
-    return  <Counter key={count.id} id={count.id} increment={increment} decrement={decrement} count={count.count}></Counter>
-   })}
+            <Counter></Counter>
+            <Counter></Counter>
+            <Counter></Counter>
+        </div>
+
     
-    <State count={totalCount()}></State>
-
-
-      </div>
-</div>
-  )
+    )
 }
 
 export default App
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,18 +1,33 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
-import Count from './Count';
-import Button from './Button';
-// eslint-disable-next-line react/prop-types
-const Counter = ({id, increment, decrement, count}) => {
+import React, { useState } from 'react';
+const Counter = () => {
+const [count,setCount] = useState(0)
+const increment = () =>{
+    setCount((prevCount) => prevCount + 1)
+}
+const decrement = () =>{
+    setCount((prevCount) => prevCount - 1)
+}
     return (
-        <div className='p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow'>
-            <Count count={count}></Count>
-
-            <div className='flex space-x-3'>
-                <Button handler={()=>increment(id)}>increment</Button>
-                <Button handler={()=>decrement(id)}>Decrement</Button>
+        
+        <div className="max-w-md mx-auto mt-10 space-y-5">
+        <div
+            className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
+        >
+            <div className="text-2xl font-semibold " id="value">{count}</div>
+            <div className="flex space-x-3">
+                <button onClick={increment} id="increment-btn"
+                    className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+                >
+                    Increment
+                </button>
+                <button onClick={decrement} id="decrement-btn"
+                    className="bg-red-400 text-white px-3 py-2 rounded shadow"
+                >
+                    Decrement
+                </button>
             </div>
         </div>
+    </div>
     );
 };
 
